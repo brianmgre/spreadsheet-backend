ab410dfbde935a57cc2796c8103137efede03e21
Brian G <brian.gregorius@gmail.com>
2019-10-02 23:46:42 -0400

    filled out model for spreadsheets

--- models/spreadsheetModel.js
+++ models/spreadsheetModel.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const SpreadSheet = Schema({}, { strict: false });
+const SpreadSheet = Schema({
+  sheetName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  data: {
+    type: String,
+    required: true
+  }
+});
 
 Notes = module.exports = mongoose.model("SpreadSheet", SpreadSheet);
